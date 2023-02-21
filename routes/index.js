0cbee3d1f459042f5a959f404f01915a3865a8ee
J1quet1-github <125953599+J1quet1@users.noreply.github.com>
2023-02-21 05:06:19 -0300

    init

---
+++ routes/index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.delete('/file', (req, res)=>{
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true 
+  });    
+
+  form.parse(req, (err, field, files)=>{
+
+    let path = field.path;
+
+    if (fs.existsSync(path)) {
+
+      fs.unlink(path, err=>{
+
+        if (err) {
+          res.status(400).json({
+            err
+          });
+        } else {
+
+          res.json({
+            field
+          });
+
+        }
+
+      });
+
+    }
+
+  });
+
+});
+
+  router.post('/upload', (req, res) => {
+
+    let form = new formidable.IncomingForm({
+      uploadDir: './upload',
+      keepExtensions: true 
+    });    
+
+    form.parse(req, (err, field, files)=>{
+
+      res.json({
+        files
+      });
+
+    });
+
+    
+
+  });
+
+module.exports = router;
