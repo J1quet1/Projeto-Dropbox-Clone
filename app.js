6f418dab6c3918919509c585b06107905b319485
J1quet1-github <125953599+J1quet1@users.noreply.github.com>
2023-05-27 00:11:05 -0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -21,10 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
